4f16d3493ed27ffc3e24c15dba30906159ee775a
Anjelica <ambrosio.anjelica@gmail.com>
2019-12-05 14:52:55 -0800

    added products into the product grid

--- app.py
+++ app.py
@@ -8,15 +8,14 @@ import os
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Clothes')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
-clothes = db.clothes
+clothes = db.Clothes
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template('index.html', clothes=clothes.find())
+    return render_template('homepage.html', clothes=clothes.find())
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-    
